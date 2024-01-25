2abd1ad50636770e343a1a8d86ba1dadd74f4578
sho6666 <104380893+sho6666@users.noreply.github.com>
2024-01-25 10:56:45 +0900

    Update DestroyItem.cs

--- Assets/DestroyItem.cs
+++ Assets/DestroyItem.cs
@@ -5,40 +5,25 @@ using UnityEngine;
 public class DestroyItem : MonoBehaviour
 
 {
-    private GameObject MainCamera;
-
-    private float difference;
-
-    private GameObject Car;
-
-    private GameObject Coin;
-
-    private GameObject TrafficCone;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.MainCamera = GameObject.Find("MainCamera");
-
-        this.Car = GameObject.Find("CarPrefab(Clone)");
-
-        this.Coin = GameObject.Find("CoinPrefab(Clone)");
-
-        this.TrafficCone = GameObject.Find("TrafficConePrefab(Clone)");
-
+        
     }
 
     private void OnBecameInvisible()
     {
-        Destroy(this.Car);
-        Destroy(this.Coin);
-        Destroy(this.TrafficCone);
+        Destroy(this.gameObject);
+       
 
     }
 }
